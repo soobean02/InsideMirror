2c8a0ff62379efd088c6bdb66d61f5e5efd7cc43
gusxo <gusxo004@naver.com>
2024-08-02 11:43:26 +0900

    fixed component

--- src/main/java/kr/co/iei/board/controller/BoardController.java
+++ src/main/java/kr/co/iei/board/controller/BoardController.java
@@ -1,10 +1,12 @@
 package kr.co.iei.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.board.model.service.BoardService;
+import kr.co.iei.utils.FileUtils;
 
 @Controller
 @RequestMapping(value="/board")
@@ -13,4 +15,9 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
+	@Value("${file.root}")
+	private String root;
+	
+	@Autowired
+	private FileUtils fileUtils;
 }
