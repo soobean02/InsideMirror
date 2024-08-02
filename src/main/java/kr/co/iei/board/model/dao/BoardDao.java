feeacf4ce8866f458e5fd86c8a36b6a015a6ca1d
gusxo <gusxo004@naver.com>
2024-08-02 19:02:29 +0900

    made board_list

--- src/main/java/kr/co/iei/board/model/dao/BoardDao.java
+++ src/main/java/kr/co/iei/board/model/dao/BoardDao.java
@@ -1,5 +1,7 @@
 package kr.co.iei.board.model.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -19,4 +21,25 @@ public class BoardDao {
 	private BoardFileRowMapper boardFileRowMapper;
 	@Autowired
 	private BoardCommentRowMapper boardCommentRowMapper;
+	
+	
+	public List selectBoardList(int start, int end) {
+		String query = "select b_tbl.*,\r\n" + //
+						"(select count(*) from board_like where board_no = b_tbl.board_no) as board_like\r\n" + //
+						"from (select rownum as rnum, b.* from (select * from board order by 1 desc)b)b_tbl where rnum between ? and ?";
+		Object[] params = {start,end};
+		List list = jdbc.query(query, boardRowMapper, params);
+		return list;
+	}//자유 게시글 10개 조회
+
+
+	public int selectBoardTotalCount() {
+		String query = "select count(*) from board";
+
+		int totalCount = jdbc.queryForObject(query, Integer.class);
+		return totalCount;
+	}//자유 게시글 총 개수 조회
+
+
+
 }
--- src/main/java/kr/co/iei/board/model/service/BoardService.java
+++ src/main/java/kr/co/iei/board/model/service/BoardService.java
@@ -1,5 +1,7 @@
 package kr.co.iei.board.model.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +15,59 @@ public class BoardService {
 	private BoardDao boardDao;
 
 	public BoardListData selectBoardList(int reqPage) {
-		
-		return null;
+		int numPerPage = 10;
+
+		int end = reqPage * numPerPage;
+		int start = end - numPerPage + 1;
+
+		List list = boardDao.selectBoardList(start, end);
+
+		int totalCount = boardDao.selectBoardTotalCount();
+
+		int totalPage = 0;
+		if(totalCount % numPerPage == 0){
+			totalPage = totalCount/numPerPage;
+		}
+		else{
+			totalPage = totalCount/numPerPage + 1;
+		}
+
+		int pageNaviSize = 5;
+		int pageNo = ((reqPage -1)/pageNaviSize) * pageNaviSize + 1;
+
+		String pageNavi = "<ul class='page-wrap'>";
+
+		if(pageNo != 1){
+			pageNavi += "<li><a class='page-index' href='/board/list?reqPage="+(pageNo - 1)+"'><span> < </span></a></li>";
+		}
+
+		for(int i = 0; i < pageNaviSize; i++){
+			pageNavi += "<li>";
+			if(pageNo == reqPage){
+				pageNavi += "<a class='page-index active-page' href='/board/list?reqPage="+pageNo+"'>";
+			}
+			else{
+				pageNavi += "<a class='page-index' href='/board/list?reqPage="+pageNo+"'>";
+			}
+			
+			pageNavi += pageNo;
+			pageNavi += "</a></li>";
+			pageNo++;
+			
+			if(pageNo > totalPage) break;
+		}//for
+
+		if(pageNo <= totalPage){
+			pageNavi += "<li>";
+			pageNavi += "<a class='page-index' href='/board/list?reqPage="+pageNo+"'>";
+			pageNavi += "<span> > </span>";
+			pageNavi += "</a></li>";
+		}
+
+		pageNavi += "</ul>";
+
+		BoardListData bld = new BoardListData(list, pageNavi);
+
+		return bld;
 	}
 }
