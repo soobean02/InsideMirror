43000d8856d5ede4ea30ef917cd2ef73e204eb7f
spring723 <khspring77@gmail.com>
2024-08-02 13:50:55 +0900

    date

--- src/main/java/kr/co/iei/product/dto/BuyProduct.java
+++ src/main/java/kr/co/iei/product/dto/BuyProduct.java
@@ -13,9 +13,9 @@ public class BuyProduct {
 	private int buyNo; // 구매한 상품 번호
 	private int productNO; // 판매 상품 번호(참조)
 	private int memberNo; // 멤버 번호(참조)
-	private String buyDate; // 구매 날짜
+	private Date buyDate; // 구매 날짜
 	
 	/*admin에서 사용하는 거*/
 	private String refundStatus; // 환불 상태 (환불/대기/사용) - admin에서 쓰는거
-	private String refundDate; // 환불 날짜 - admin에서 사용하는거
+	private Date refundDate; // 환불 날짜 - admin에서 사용하는거
 }
--- src/main/java/kr/co/iei/product/dto/BuyProductRowMapper.java
+++ src/main/java/kr/co/iei/product/dto/BuyProductRowMapper.java
@@ -12,9 +12,9 @@ public class BuyProductRowMapper implements RowMapper<BuyProduct>{
 	@Override
 	public BuyProduct mapRow(ResultSet rs, int rowNum) throws SQLException {
 		BuyProduct bp = new BuyProduct();
-		bp.setRefundDate(rs.getString("refund_date"));
+		bp.setRefundDate(rs.getDate("refund_date"));
 		bp.setRefundStatus(rs.getString("refund_status"));
-		bp.setBuyDate(rs.getString("buy_date"));
+		bp.setBuyDate(rs.getDate("buy_date"));
 		bp.setMemberNo(rs.getInt("member_no"));
 		bp.setProductNO(rs.getInt("product_no"));
 		bp.setBuyNo(rs.getInt("buy_no"));
