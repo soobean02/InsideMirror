782c51334e9e275ab582808f4ecb8b03fbcd2af5
khj0114 <dulli160@naver.com>
2024-08-05 17:30:19 +0900

    커밋

--- src/main/java/kr/co/iei/guestbook/controller/GuestBookController.java
+++ src/main/java/kr/co/iei/guestbook/controller/GuestBookController.java
@@ -3,8 +3,12 @@ package kr.co.iei.guestbook.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.co.iei.guestbook.dto.GuestBook;
 import kr.co.iei.guestbook.service.GuestBookService;
 import kr.co.iei.utils.FileUtils;
 
@@ -19,5 +23,53 @@ public class GuestBookController {
 	
 	@Autowired
 	private FileUtils fileUtils;
+	
+	
+@PostMapping(value="/insertComment")
+public String insertComment(GuestBook gb, Model model) {
+	System.out.println(111);
+	int result = guestBookService.insertComment(gb);
+	if(result > 0) {	
+	model.addAttribute("title", "댓글작성");
+	model.addAttribute("msg", "댓글이 작성되었습니다");
+	model.addAttribute("icon", "success");
+}else {
+	model.addAttribute("title", "댓글작성 실패");
+	model.addAttribute("msg", "댓글이 작성중 문제가 생겼습니다");
+	model.addAttribute("icon", "warning");
+}
+	model.addAttribute("loc", "/guest/guestBook?memberNo");
+	return "common/msg";
+}
+@PostMapping(value="/updateComment")
+public String updateComment(GuestBook gb ,Model model) {
+	int result = guestBookService.updateComment(gb);
+	if(result > 0) {
+		model.addAttribute("title", "성공");
+		model.addAttribute("msg", "댓글이 수정되었습니다.");
+		model.addAttribute("icon", "success");
+	}else {
+		model.addAttribute("title", "실패");
+		model.addAttribute("msg", "잠시후 다시 시도해주세요.");
+		model.addAttribute("icon", "warning");
+	}
+	model.addAttribute("loc", "/guest/guestBook?check=1&memberNo");
+	return "common/msg";
+}
+@GetMapping(value="/deleteComment")
+public String deleteComment(GuestBook gb ,Model model) {
+	int result = guestBookService.deleteComment(gb);
+	if(result > 0) {
+		model.addAttribute("title", "성공");
+		model.addAttribute("msg", "댓글이 삭제되었습니다.");
+		model.addAttribute("icon", "success");
+	}else {
+		model.addAttribute("title", "실패");
+		model.addAttribute("msg", "잠시후 다시 시도해주세요.");
+		model.addAttribute("icon", "warning");
+	}
+	model.addAttribute("loc", "/guest/guestBook?check=1&memberNo=");
+	return "common/msg";
+}
 
 }
--- src/main/java/kr/co/iei/guestbook/dao/GuestBookDao.java
+++ src/main/java/kr/co/iei/guestbook/dao/GuestBookDao.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import kr.co.iei.guestbook.dto.GuestBook;
+
 import kr.co.iei.guestbook.dto.GuestBookRowMapper;
 @Repository
 public class GuestBookDao {
@@ -11,4 +13,25 @@ public class GuestBookDao {
 	private JdbcTemplate jdbc;
 	@Autowired
 	private GuestBookRowMapper guestBookRowMapper;
+	
+	public int insertComment(GuestBook gb) {
+		String query = "insert into guestbook values(guestbook_seq.nextval,?,to char(sysdate,'yyyy-mm-dd)";
+		Object[] params = {gb.getGuestWriterNo()};
+		int result = jdbc.update(query, params);
+		return result;
+	}
+
+	public int updateComment(GuestBook gb) {
+		String query = "update guestbook set guestbook_comment_content=? where guestbook_comment_no=?";
+		Object[] params = {gb.getGuestCommentContent(),gb.getGuestCommentNo()};
+		int result = jdbc.update(query, params);
+		return result;
+	}
+
+	public int deleteComment(GuestBook gb) {
+		String query = "delete from guestbook where guestbook_comment_no=?";
+		Object[] params = {gb.getGuestCommentNo()};
+		int result = jdbc.update(query, params);
+		return result;
+	}
 }
