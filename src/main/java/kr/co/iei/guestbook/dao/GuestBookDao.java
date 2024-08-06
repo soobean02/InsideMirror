56b9d3f6dd542ee0a8d34d8784502ffaee74765b
khj0114 <dulli160@naver.com>
2024-08-06 09:27:53 +0900

    커밋

--- src/main/java/kr/co/iei/guestbook/dao/GuestBookDao.java
+++ src/main/java/kr/co/iei/guestbook/dao/GuestBookDao.java
@@ -3,35 +3,33 @@ package kr.co.iei.guestbook.dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
-
 import kr.co.iei.guestbook.dto.GuestBook;
-
 import kr.co.iei.guestbook.dto.GuestBookRowMapper;
+
 @Repository
 public class GuestBookDao {
-	@Autowired
-	private JdbcTemplate jdbc;
-	@Autowired
-	private GuestBookRowMapper guestBookRowMapper;
-	
-	public int insertComment(GuestBook gb) {
-		String query = "insert into guestbook values(guestbook_seq.nextval,?,to char(sysdate,'yyyy-mm-dd)";
-		Object[] params = {gb.getGuestWriterNo()};
-		int result = jdbc.update(query, params);
-		return result;
-	}
+    @Autowired
+    private JdbcTemplate jdbc;
+    
+    @Autowired
+    private GuestBookRowMapper guestBookRowMapper;
+    
+    public int insertComment(GuestBook gb) {
+        String query = "INSERT INTO guestbook (guestbook_comment_no, guest_writer_no, guest_comment_content, guest_comment_date) " +
+                       "VALUES (guestbook_seq.nextval, ?, ?, SYSDATE)";
+        Object[] params = { gb.getGuestWriterNo(), gb.getGuestCommentContent() };
+        return jdbc.update(query, params);
+    }
 
-	public int updateComment(GuestBook gb) {
-		String query = "update guestbook set guestbook_comment_content=? where guestbook_comment_no=?";
-		Object[] params = {gb.getGuestCommentContent(),gb.getGuestCommentNo()};
-		int result = jdbc.update(query, params);
-		return result;
-	}
+    public int updateComment(GuestBook gb) {
+        String query = "UPDATE guestbook SET guest_comment_content = ? WHERE guestbook_comment_no = ?";
+        Object[] params = { gb.getGuestCommentContent(), gb.getGuestCommentNo() };
+        return jdbc.update(query, params);
+    }
 
-	public int deleteComment(GuestBook gb) {
-		String query = "delete from guestbook where guestbook_comment_no=?";
-		Object[] params = {gb.getGuestCommentNo()};
-		int result = jdbc.update(query, params);
-		return result;
-	}
-}
+    public int deleteComment(GuestBook gb) {
+        String query = "DELETE FROM guestbook WHERE guestbook_comment_no = ?";
+        Object[] params = { gb.getGuestCommentNo() };
+        return jdbc.update(query, params);
+    }
+} 
