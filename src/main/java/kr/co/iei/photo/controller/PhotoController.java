5fb2324928e366792687a242f7dc2689adc6cb47
gusxo <gusxo004@naver.com>
2024-08-09 10:27:33 +0900

    fixed weird tab

--- src/main/java/kr/co/iei/photo/controller/PhotoController.java
+++ src/main/java/kr/co/iei/photo/controller/PhotoController.java
@@ -2,6 +2,8 @@ package kr.co.iei.photo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.iei.photo.model.service.PhotoService;
@@ -11,4 +13,10 @@ import kr.co.iei.photo.model.service.PhotoService;
 public class PhotoController {
 	@Autowired
 	private PhotoService photoService;
+
+	@GetMapping(value="list")
+	public String list(Model model){
+
+		return "/photo/list";
+	}
 }
