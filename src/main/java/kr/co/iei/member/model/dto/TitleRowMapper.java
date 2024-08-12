286e4eccc9002b7ff6fc8a28b9c72e99fa47466f
gusxo <gusxo004@naver.com>
2024-08-12 10:05:54 +0900

    수정

--- src/main/java/kr/co/iei/member/model/dto/TitleRowMapper.java
+++ src/main/java/kr/co/iei/member/model/dto/TitleRowMapper.java
@@ -14,24 +14,24 @@ public class TitleRowMapper implements RowMapper<Title>{
 	@Override
 	public Title mapRow(ResultSet rs, int rowNum) throws SQLException {
 	
-		Photo p = new Photo();
-		p.setMemberNo(rs.getInt("member_no"));
-		p.setPhotoContent(rs.getString("photo_content"));
-		p.setPhotoDate(rs.getDate("photo_Date"));
-		p.setPhotoNo(rs.getInt("photo_no"));
-		p.setPhotoTitle(rs.getString("photo_title"));
-		
-		Board b = new Board();
-		b.setBoardContent(rs.getString("board_content"));
-		b.setBoardDate(rs.getString("board_date"));
-		b.setBoardTitle(rs.getString("board_title"));
-		b.setBoardNo(rs.getInt("board_no"));
-		b.setMemberNo(rs.getInt("member_no"));
-				
-		Title title = new Title();
+//		Photo p = new Photo();
+//		p.setMemberNo(rs.getInt("member_no"));
+//		p.setPhotoContent(rs.getString("photo_content"));
+//		p.setPhotoString(rs.getString("photo_Date"));
+//		p.setPhotoNo(rs.getInt("photo_no"));
+//		p.setPhotoTitle(rs.getString("photo_title"));
+//		
+//		Board b = new Board();
+//		b.setBoardContent(rs.getString("board_content"));
+//		b.setBoardDate(rs.getString("board_date"));
+//		b.setBoardTitle(rs.getString("board_title"));
+//		b.setBoardNo(rs.getInt("board_no"));
+//		b.setMemberNo(rs.getInt("member_no"));
+//				
+//		Title title = new Title();
 //		title.setBoard(b);
 //		title.setPhoto(p);
 		//useless class 임
-		return title;
+		return null;
 	}
 }
